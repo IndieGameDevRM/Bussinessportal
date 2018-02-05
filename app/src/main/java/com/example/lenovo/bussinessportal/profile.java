2fd71d0b68fdb1a3f26d6ce0c03e63a8c0676237
RishabhMatharoo11 <rishabhmatharoo11@gmail.com>
2018-02-05 18:15:25 +0530

    firebasedatabase added insert operation added

--- app/src/main/java/com/example/lenovo/bussinessportal/profile.java
+++ app/src/main/java/com/example/lenovo/bussinessportal/profile.java
@@ -8,44 +8,88 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class profile extends AppCompatActivity implements View.OnClickListener{
+    private Button add;
     private Button logout;
-    private FirebaseAuth firebaseAuth;
+    private EditText name;
+    private EditText contactnumber;
+    private EditText additionaldetail;
     private ProgressDialog progressDialog;
-    //private DrawerLayout mDrawlayout;
-    //private ActionBarDrawerToggle mtoggle;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseUser;
+    private profile_database data=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        logout=(Button)findViewById(R.id.nav_logout);
-        firebaseAuth=FirebaseAuth.getInstance();
-        progressDialog=new ProgressDialog(this);
-        logout.setOnClickListener(this);
-        //mDrawlayout=(DrawerLayout)findViewById(R.id.drawer);
-        //mtoggle=new ActionBarDrawerToggle(this,mDrawlayout,R.string.Open,R.string.Close);
-       // mtoggle.syncState();
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
-    /*@Override
-     public boolean onOptionsItemSelected(MenuItem item){
-        if(mtoggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
+        //button declaration
+
+        logout = (Button) findViewById(R.id.nav_logout);
+        add=(Button)findViewById(R.id.profile_insert);
+
+        //editText declaration
+        name=(EditText)findViewById(R.id.profile_name);
+        contactnumber=(EditText)findViewById(R.id.profile_ContactNo);
+        additionaldetail=(EditText)findViewById(R.id.profile_detail);
+
+        //firebase authentication declaration
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //declaring database
+
+        databaseUser= FirebaseDatabase.getInstance().getReference("User");
+        //progress dialog declaration
+
+        progressDialog = new ProgressDialog(this);
+
+        //adding listener on logout and add buttons
+
+        logout.setOnClickListener(this);
+        add.setOnClickListener(this);
+    }
     private void Logout_User(){
         firebaseAuth.signOut();
         progressDialog.dismiss();
         Intent i=new Intent(this,loginActivity.class);
         startActivity(i);
+    }
+    private void insert_in_firebase_database(){
+        String contactno=contactnumber.getText().toString().trim();
+        String pro_name=name.getText().toString().trim();
+        String detail=additionaldetail.getText().toString().trim();
+
+        if(TextUtils.isEmpty(contactno)){
+            //if text field of name will be empty
+            Toast.makeText(this,"Please fill the form!",Toast.LENGTH_SHORT).show();
+            return;
+        }else if(TextUtils.isEmpty(pro_name)){
+            //if text field of name will be empty
+            Toast.makeText(this,"Please fill the form!",Toast.LENGTH_SHORT).show();
+            return;
+        }else if(TextUtils.isEmpty(detail)){
+            //if text field of name will be empty
+            Toast.makeText(this,"Please fill the form!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //now data will insert in the database
+        String id=databaseUser.push().getKey();//it will generate a primary key and store its value in id;
+        data=new profile_database(pro_name,contactno,detail,id);
+        databaseUser.child(id).setValue(data);
+        Toast.makeText(this,"successful!", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onClick(View view){
@@ -53,6 +97,9 @@ public class profile extends AppCompatActivity implements View.OnClickListener{
             progressDialog.setMessage("Logging out......");
             progressDialog.show();
             Logout_User();
+        }
+        if(view == add){
+            insert_in_firebase_database();
         }
     }
 }
